32cd134a8cd7452e9631bddfc1d2fbe4c7685e0a
Dino Nguyen and Justin Haupt <justin@>
2019-02-04 15:44:02 -0800

    add styles for search result

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+var page = 0;
+
+function getEvents(keyword) {
+
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&keyword=" + keyword + "&dmaid=362&size=4&page=" + page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+          showEvents(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+
+function showEvents(json) {
+
+  console.log(json);
+
+}
+
+
+
+
+
+$(function() {
+
+
+  $("#search").click(function() {
+     var keyword = $("#bar").val();
+     getEvents(keyword);
+  });
+
+});
