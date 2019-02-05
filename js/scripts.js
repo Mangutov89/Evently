e2d559c363ac37cab50676eb6e4f5d355b29a39b
David Monarrez and Dino Nguyen <dino@>
2019-02-05 11:36:07 -0800

    Add search results functionality and formatting

--- js/scripts.js
+++ js/scripts.js
@@ -18,26 +18,39 @@ function getEvents(keyword) {
   });
 }
 
+function convertTime(time) {
+  let timeSplit = time.split(":");
+  timeSplit.pop();
+  let convertedTime;
+  if(Number(timeSplit[0]) > 12) {
+    timeSplit[0] -= 12;
+    timeSplit[0].toString();
+    convertedTime = timeSplit.join(":") + " PM";
+  } else {
+    convertedTime =  timeSplit.join(":") + " AM";
+  }
+  return convertedTime;
+}
 
 function showEvents(json) {
-  let events = json._embedded.events
-  events.forEach(function (event) {
-    console.log(event);
-  });
-  //console.log(events);
+  let events = json._embedded.events;
+  // for each event...
+  events.forEach(function (event){
+    // debugger;
+    // add list item
+    $('.results').append('<li class="result-item"><div class="test img-contianer"><img class="test-image" src="' + event.images[0].url + '"</img></div> <div class="test test-content"><h2 class="event-title">' + event.name + '</h2><p class="event-description">' + event._embedded.venues[0].name + '</p><p class="event-date">' + event.dates.start.localDate + '</p><p class="event-date">' + convertTime(event.dates.start.localTime) + '</p></div></li>');
 
+  });
+  console.log(events);
+  console.log(events[0].images[0].url);
 }
 
 
 
 
-
-$(function() {
-
-
+$(document).ready(function () {
   $("#search").click(function() {
-     var keyword = $("#bar").val();
-     getEvents(keyword);
+    var keyword = $("#bar").val();
+    getEvents(keyword);
   });
-
 });
