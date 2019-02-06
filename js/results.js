44b914ec61a29648820d22ee67512bd7a4cd0bad
Dino Nguyen and Justin Haupt <justin@>
2019-02-06 10:27:09 -0800

    Link index.html search keyword to results page

---
+++ js/results.js
@@ -0,0 +1,109 @@
+// $(function () {
+//     var body = $('body');
+//     var backgrounds = [
+//       'url(img/crowd-bg2.jpg)',
+//       'url(img/megan.jpg)'];
+//     var current = 0;
+//
+//     function nextBackground() {
+//         body.css(
+//             'background',
+//         backgrounds[current = ++current % backgrounds.length]);
+//
+//         setTimeout(nextBackground, 5000);
+//     }
+//     setTimeout(nextBackground, 5000);
+//     body.css('background', backgrounds[0]);
+//     body.addClass('.image-settings');
+// });
+
+
+
+var page = 0;
+
+function getEvents(keyword) {
+
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&keyword=" + keyword + "&stateCode=OR&city=portland&size=30&page=" + page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+          showEvents(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+function convertTime(time) {
+  let timeSplit = time.split(":");
+  timeSplit.pop();
+  let convertedTime;
+  if(Number(timeSplit[0]) > 12) {
+    timeSplit[0] -= 12;
+    timeSplit[0].toString();
+    convertedTime = timeSplit.join(":") + " PM";
+  } else {
+    convertedTime =  timeSplit.join(":") + " AM";
+  }
+  return convertedTime;
+}
+
+function checkIfUndefined(elementArr) {
+  for(let i = 0; i < elementArr.length; i++){
+    if(elementArr[i] === undefined) {
+      elementArr[i] = '-';
+    }
+  }
+  // elementArr.forEach(function (element) {
+  // });
+}
+
+function showEvents(json) {
+  let events = json._embedded.events;
+  $('.results').remove();
+  $('.col-xs-8').append('<ul class="results"></ul>');
+  console.log(events);
+
+  events.forEach(function (event){
+    let elementsArr = [event.images, event.name, event._embedded.venues, event.dates.start.localDate, event.dates.start.localTime, event.priceRanges];
+    checkIfUndefined(elementsArr);
+    //dependant
+    let images = elementsArr[0];
+    let venues = elementsArr[2];
+    let priceRange = elementsArr[5];
+    //independent
+    let eventName = elementsArr[1];
+    let eventDate = elementsArr[3];
+    let eventTime = elementsArr[4];
+
+
+    let imageURL = (event.images) ? event.images[0].url : 'img/e-logo.png';
+    let venueName = (event._embedded.venues) ? event._embedded.venues[0].name :'-';
+    let priceMin = (event.priceRanges) ? event.priceRanges[0].min : '-';
+    let priceMax = (event.priceRanges) ? event.priceRanges[0].max : '-';
+
+    // add list item
+    $('.results').append('<li class="result-item"><div class="test img-container"><img class="test-image" src="' + imageURL + '"</img></div> <div class="test test-content"><h2 class="event-title">' + eventName + '</h2><p class="event-description">' + venueName + '</p><p class="event-date">' + eventDate + '</p><p class="event-date">' + convertTime(eventTime) + '</p><p class="price">Prices From: $' + priceMin + ' to $' + priceMax + '</p></div></li>');
+  });
+}
+
+
+
+
+$(document).ready(function () {
+
+  // display search results when navigate to page
+  console.log(localStorage.getItem("key"));
+  getEvents(localStorage.getItem("key"));
+
+  $(".search").click(function() {
+    var keyword = $(".bar").val();
+        //console.log("hi");
+    getEvents(keyword);
+  });
+
+});
